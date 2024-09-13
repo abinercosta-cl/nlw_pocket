2da3b3d038c08f89e38043b54f8a1ef9263999a7
abinercosta-cl <abinerclcosta@gmail.com>
2024-09-13 16:59:45 -0300

    criado arquivo de metas.json e sistema para salvar metas no arquivo

--- app/index.js
+++ app/index.js
@@ -1,13 +1,24 @@
 const {select, input, checkbox} = require('@inquirer/prompts')
+const fs = require("fs").promises
 
 let mensagem = "Bem-vindo ao app de metas";
 
-let meta = {
-    value: "Treinar todo dia",
-    checked: false,
+let metas 
+
+const carregarMetas = async () => {
+    try{
+        const dados = await fs.readFile("metas.json", "utf-8")
+        metas = JSON.parse(dados)
+    }
+    catch(erro){
+        metas = []
+    }    
 }
 
-let metas = [meta]
+const salvarMetas = async () => {
+    await fs.writeFile("metas.json", JSON.stringify(metas, null, 2))
+}
+
 
 const cadastrarMeta = async () => {
     const meta = await input({message: "Digite a Meta:"})
@@ -128,9 +139,11 @@ const mostrarMensagem =  () => {
 
 async function start  () {
 
+    await carregarMetas()
+
     while(true){
         mostrarMensagem()
-        
+        await salvarMetas()
         
         const opcao = await select({
             message: "Menu >",
